cb39391d9fdb8a01a999ae341cfa83f3221a4c1f
Wuy <2281137167@qq.com>
2026-09-16 14:47:45 +0800

    更新评估和训练脚本，添加traj_fusion参数

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,6 +1,6 @@
 
 CUDA_VISIBLE_DEVICES=2 python eval.py --batch_size 1 --sample_points 4096 \
- --load_model_dir /data/wuyang/output/motion_ckpts/best.pth "$@"
+ --load_model_dir /data/wuyang/output/motion_ckpts/best.pth --traj_fusion"$@"
 
 
 
--- scripts/train.sh
+++ scripts/train.sh
@@ -7,4 +7,4 @@ fi
 export CUDA_VISIBLE_DEVICES=$DEVICE
 
 python train.py --save_fre 1 --val_fre 1 --batch_size 8 --sample_points 4096 \
-    --dropout 0.3 --epoch 100 --occ "$@"
+    --dropout 0.3 --epoch 100 --occ --traj_fusion "$@"
